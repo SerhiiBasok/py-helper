324a00a701126e2876278ecf98bf0fb2be070a90
Serhii Basok <sergeybsk19@gmail.com>
2025-08-27 20:15:02 +0300

    add develop and prod files, connect to PostgreSQL

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from advertisements.views import HomePageView
@@ -47,3 +49,6 @@ urlpatterns = [
         )
     ),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
